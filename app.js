f6553a200c2cf28e353aa7a6f0876551457e8606
tianyuyzip <30584005+tianyuyzip@users.noreply.github.com>
2017-09-16 21:57:30 +0800

    加载所有签到信息， 根据学号查找学生的个人签到记录， 分页查询签到记录

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var router = require("./controller/router.js");
 
 // 显示欢迎首页
 app.all('*', function(req, res, next) {
-    console.log("access control here!");
+    // console.log("access control here!");
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
@@ -16,6 +16,10 @@ app.get("/v1/stu/:stuno",router.getStuByNo);
 app.get("/v1/stu/signin/:stuno",router.loadAllAttHistory);
 app.get("/v1/stu/course/:date",router.loadTodayCourses);
 app.get("/v1/stu/signCnt/:stuno/:status",router.updateSignCnt);
+app.post("/v1/stu/signinfo/info",router.addSigninfo);
+app.get("/v1/stu/signinfo/:stuno/:courseno/:date/:begintime",router.getSignedinfo);
+app.get("/v1/stu/signinfo/:stuno",router.loadAllSignedRecord);
+app.get("/v1/stu/signinfo/:stuno/:pageNo/:pageSize",router.loadScopedSignedRecord);
 
 app.listen(80,function(){
   //console.log(__dirname);
--- controller/router.js
+++ controller/router.js
@@ -6,12 +6,12 @@ var getStuByNo = function (req,res) {
     var username = req.params;
     // console.log(username);
     db.query("student",username,1,1,{},function (err,result) {
-        console.log(result);
+        // console.log(result);
         if(err){
-            console.log("err result");
+            // console.log("err result");
             res.json({"result":1});
         }else{
-            console.log(result[0]);
+            // console.log(result[0]);
             res.json({"result":result[0]});
         }
     });
@@ -25,7 +25,7 @@ var loadAllAttHistory = function (req,res) {
         if(err){
             res.json({"result":1});
         }else{
-            console.log(result);
+            // console.log(result);
             res.json({"result":result});
         }
 
@@ -36,12 +36,12 @@ var loadTodayCourses = function (req,res) {
 
     var date = req.params;
 
-    db.query("course",date,4,1,{begintime:1},function (err,result) {
+    db.query("course",date,5,1,{begintime:1},function (err,result) {
 
         if(err){
             res.json({"result":1});
         }else{
-            console.log(result);
+            // console.log(result);
             res.json({"result":result});
         }
     })
@@ -56,7 +56,16 @@ var updateSignCnt = function (req,res) {
           res.json({"result":1});
       }else{
           if(result){
-
+              db.update("signCnt",{stuno:stuno},{$set:getDocByStatus(status,result[0])},function (n) {
+                  // console.log("signincnt成功更新" + n + "条记录");
+                  db.query("signCnt",{stuno:stuno},1,1,{},function (err,result) {
+                      if(err){
+                          res.json({"result":1});
+                      }else{
+                          res.json({"result":result});
+                      }
+                  })
+              })
           }
       }
   })
@@ -65,23 +74,78 @@ var updateSignCnt = function (req,res) {
 function getDocByStatus(status,result){
     var doc = null;
     if(status === "待签到"){
-        var normalcnt = parseInt(result.normalcnt)+1
+        var normalcnt = parseInt(result.normalcnt)+1;
         doc = {"normalcnt":normalcnt};
     }
     else if(status === "迟到"){
-        var latecnt = parseInt(result.latecnt)+1
+        var latecnt = parseInt(result.latecnt)+1;
         doc = {"latecnt":latecnt};
     }
     else if(status === "缺勤"){
-        var absencecnt = parseInt(result.absencecnt)+1
-        doc = {"absentcnt":absencecnt};
+        var absentcnt = parseInt(result.absentcnt)+1;
+        doc = {"absentcnt":absentcnt};
     }
 
     return doc;
+};
+
+var addSigninfo = function (req,res) {
+
+    var info = JSON.parse(req.query.info);
+    db.insertOne("signinfo",info,function (err) {
+        if(err)
+            res.json({"result":1});
+        else
+            res.json({"result":"success"});
+    })
+};
+
+var getSignedinfo = function (req,res) {
+    var stuno = req.params.stuno;
+    var courseno = req.params.courseno;
+    var date = req.params.date;
+    var begintime = req.params.begintime;
+
+    // console.log(stuno+courseno+date+begintime);
+    db.query("signinfo",{stuno:stuno,courseno:courseno,signdate:date,begintime:begintime},1,1,{},function (err,result) {
+        if(result.length!=0) {
+            // console.log(result);
+            res.json({"result": result});
+        }else{
+            res.json({"result":1});
+        }
+    })
+};
+
+var loadAllSignedRecord = function (req,res) {
+    db.query("signinfo",req.params,0,1,{"signdate":1,"signtime":1},function (err,result) {
+        if(err){
+            res.json({"result":1});
+        }else{
+            res.json({"result":result});
+        }
+    })
+};
+
+var loadScopedSignedRecord = function (req,res) {
+    var stuno = req.params.stuno;
+    var pageNo = parseInt(req.params.pageNo);
+    var pageSize = parseInt(req.params.pageSize);
+    db.query("signinfo",{stuno:stuno},pageSize,pageNo,{"signdate":1,"signtime":1},function (err,result) {
+        if(err){
+            res.json({"result":1});
+        }else{
+            res.json({"result":result});
+        }
+    })
 }
 
 exports.getStuByNo = getStuByNo;
 exports.loadAllAttHistory = loadAllAttHistory;
 exports.loadTodayCourses = loadTodayCourses;
 exports.updateSignCnt = updateSignCnt;
+exports.addSigninfo = addSigninfo;
+exports.getSignedinfo = getSignedinfo;
+exports.loadAllSignedRecord = loadAllSignedRecord;
+exports.loadScopedSignedRecord = loadScopedSignedRecord;
 
